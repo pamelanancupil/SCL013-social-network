47a7c82d1b4ac5cc3cb879f260f8a0f28505f1f1
AleKristen <correo@gmail.com>
2020-06-01 15:34:34 -0400

    intentar configurar firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { myFunction, signInUser } from './lib/index.js';
 
 myFunction();
+
+
+const signInBtn = document.getElementById("signIn");
+signInBtn.addEventListener ("click",() => {
+    let name= document.getElementById("name").value;
+    let email= document.getElementById("email").value;
+    let password= document.getElementById("password").value;
+    
+  });
+  
