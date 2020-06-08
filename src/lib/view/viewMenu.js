72a93f0d1e6776a2a91b4d17f61463b43b31ef48
AleKristen <correo@gmail.com>
2020-06-08 09:51:08 -0400

    menu css

--- src/lib/view/viewMenu.js
+++ src/lib/view/viewMenu.js
@@ -17,6 +17,18 @@ export const viewMenu = () => {
               </button>
             </div>
 
+            <div class="newPost" id="newPost">
+    <div class="title">
+      <button id="btnClosePost" class="btnClosePost"> <i class="fas fa-times"></i> </button> 
+      <p>Crear publicación</p>
+      <button id="share">Compartir</button>
+    </div>
+    <div class="text">
+      <textarea id="writeText">Escribe tu publicación</textarea>
+      <button id="btnImage" class="btnImage"> <i class="far fa-images"></i> </button>
+    </div>
+  </div>
+
       `;
 
   divMenu.setAttribute('id', 'containerMenu');
