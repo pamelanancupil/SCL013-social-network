46b40a62dcfbf49b05f615d4c796c89b7a380e28
AleKristen <correo@gmail.com>
2020-06-10 09:37:09 -0400

    responsive tablet

--- src/lib/view/viewFeed.js
+++ src/lib/view/viewFeed.js
@@ -3,16 +3,16 @@ import { showMenu } from './viewMenu.js';
 export const viewFeed = () => {
     const divFeed = document.createElement('div');
     divFeed.innerHTML = `
-  
-    <header id="headerFeed">
-        <img src="http://imgfz.com/i/TKBv2dp.png" width="50px">
-    </header>          
     
-    <div class="containerPost" id="containerPost">
-                  
-    </div>
-    <footer id="footerMenu">
-    </footer>
+        <header id="headerFeed">
+            <img src="http://imgfz.com/i/TKBv2dp.png" width="50px">
+        </header>          
+        
+        <div class="containerPost" id="containerPost">
+                    
+        </div>
+        <footer id="footerMenu">
+        </footer>
   
         `;
   
