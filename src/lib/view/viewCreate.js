a53d3b437ba7aea642f072dc2f20ba7a56eef864
pamelanancupil <pamela.nancupilv@gmail.com>
2020-06-10 18:14:25 -0400

    guardar post

--- src/lib/view/viewCreate.js
+++ src/lib/view/viewCreate.js
@@ -1,4 +1,7 @@
 import { showMenu } from './viewMenu.js';
+import { createPost } from '../firebase/firebase.js';
+import { showPost } from '../firebase/firebase.js';
+
 
 export const viewCreate = () => {
   const divCreate = document.createElement('div');
@@ -10,13 +13,14 @@ export const viewCreate = () => {
                     <a href="#/feed" id="btnClosePost" class="btnClosePost"> <i class="fas fa-times"></i> </a> 
                     <p class="share">Crear publicación</p>
                   </div>
-                    <button class="share">Compartir</button>
+                    <button id="share" class="share">Compartir</button>
                      
                 </div>
                 <div class="text">
                     <textarea id="writeText">Escribe tu publicación</textarea>
                     <button id="btnImage" class="btnImage"> <i class="far fa-images"></i> </button>
                 </div>
+                <p id="showingPost"></p>
             </div>
             <footer id="footerMenu">
             </footer>
@@ -25,5 +29,18 @@ export const viewCreate = () => {
   divCreate.setAttribute('id', 'containerCreate');
   const footer = divCreate.querySelector('#footerMenu');
     footer.appendChild(showMenu());
+
+    const btnShare = divCreate.querySelector('#share');
+    btnShare.addEventListener('click', () =>{
+    const contentText = divCreate.querySelector('#writeText').value;
+    const showingPost= divCreate.querySelector('#showingPost');
+      createPost(contentText);
+      showingPost.innerHTML += showPost(contentText);
+      console.log(contentText)
+    });
+
+
+
+
   return divCreate;
 };
