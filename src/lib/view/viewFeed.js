5d45bdaea1535f45233c18dac98dfcead1d8b42b
EliHidalgo <hidalgo_eliana@hotmail.com>
2020-06-15 16:55:05 -0400

    autenticacion en firebase

--- src/lib/view/viewFeed.js
+++ src/lib/view/viewFeed.js
@@ -1,7 +1,12 @@
 import { showMenu } from './viewMenu.js';
+import { db, getCurrentUser } from '../firebase/firebaseAuth.js';
 
 export const viewFeed = () => {
     const divFeed = document.createElement('div');
+    let user = getCurrentUser();
+    if(user) {
+
+    
     divFeed.innerHTML = `
     
         <header id="headerFeed" class="headerFCP">
@@ -9,6 +14,7 @@ export const viewFeed = () => {
         </header>          
         
         <div class="containerFeedBody" id="containerFeedBody">
+        <div id="postFeed"></div>
                     
         </div>
         <footer id="footerMenu">
@@ -19,6 +25,21 @@ export const viewFeed = () => {
     divFeed.setAttribute('id', 'containerFeed');
     const footer = divFeed.querySelector("#footerMenu");
     footer.appendChild(showMenu());
-    return divFeed;
-    
+
+    const showingPost = divFeed.querySelector('#postFeed');
+    db.collection("post").onSnapshot((querySnapshot) => {
+        showingPost.innerHTML = '';
+          querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data().content}`);
+            showingPost.innerHTML += `
+            <p id='printPost'>${doc.data().content}</p>
+            <h5 id='date'>${doc.data().date}</h5>`; 
+           
+      });
+    });
+    //return divFeed;
+    } else {
+        window.location.hash = '#/home';
+    }
+   return divFeed;    
   };
