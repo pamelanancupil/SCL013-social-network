38ec1b9efcbce2a4189c713027e3eac4b5f7d394
pamelanancupil <pamela.nancupilv@gmail.com>
2020-06-21 17:43:24 -0400

    Eslint

--- src/lib/firebase/firebaseAuth.js
+++ src/lib/firebase/firebaseAuth.js
@@ -1,6 +1,24 @@
 export const db = firebase.firestore();
 
-//REGISTRARSE CON CORREO Y CONTRASEÑA -> USUARIO NUEVO
+//  ENVIAR CORREO DE VERIFICACIÓN PARA USUARIOS CON CORREO Y CONTRASEÑA
+const verificationEmail = () => {
+  const user = firebase.auth().currentUser;
+  user.sendEmailVerification()
+    .then(() => {
+      alert('Por favor revisa tu correo y verifica tu cuenta para iniciar sesión');
+      //  window.location.hash = '#/login'; // Email sent.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
+
+//  FUNCIÓN PARA OBETER EL USUARIO ACTUAL
+export const getCurrentUser = () => {
+  return firebase.auth().currentUser;
+};
+
+//  REGISTRARSE CON CORREO Y CONTRASEÑA -> USUARIO NUEVO
 export const registerUser = (email, password, userName, onSuccess, onError) => {
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((result) => {
@@ -8,79 +26,67 @@ export const registerUser = (email, password, userName, onSuccess, onError) => {
       onSuccess(result);
     })
     .then(() => {
-      let user = getCurrentUser();
+      const user = getCurrentUser();
       return db.collection('users').doc(user.uid).set({
         name: userName,
         email: email,
         userId: user.uid,
-        description: ''
-      })
+        description: '',
+      });
     })
     .catch((error) => {
       // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      //console.log(errorMessage);
-      //console.log(errorCode);
+      //  const errorCode = error.code;
+      //  const errorMessage = error.message;
+      //  console.log(errorMessage);
+      //  console.log(errorCode);
       onError(error);
     });
 };
 
-//FUNCIÓN DE ACTUALIZAR PERFIL
+//  FUNCIÓN DE ACTUALIZAR PERFIL
 export const updateUserProfile = (userName, descriptionUser, photoURL, onSuccess, onError) => {
   const actualUser = getCurrentUser();
   actualUser.updateProfile({
     displayName: userName,
     description: descriptionUser,
-    photoURL: photoURL
+    photoURL: photoURL,
   }).then((result) => {
-    onSuccess(result)
+    onSuccess(result);
   }).catch((error) => {
     onError(error);
   });
 };
 
-//observador auth de estado de autenticación para obtener datos del usuario, se llama cada vez que cambia el estado de acceso del usuario 
+//  observador auth de estado de autenticación para obtener datos del usuario,
+//  se llama cada vez que cambia el estado de acceso del usuario
 export const authState = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      //console.log(user, 'usuario');
+      //  console.log(user, 'usuario');
       // User is signed in.
-      let displayName = user.displayName;
-      let email = user.email;
-      let emailVerified = user.emailVerified;
-      let photoURL = user.photoURL;
-      let isAnonymous = user.isAnonymous;
-      let uid = user.uid;
-      let providerData = user.providerData;
+      const displayName = user.displayName;
+      const email = user.email;
+      const emailVerified = user.emailVerified;
+      const photoURL = user.photoURL;
+      const isAnonymous = user.isAnonymous;
+      const uid = user.uid;
+      const providerData = user.providerData;
       if (emailVerified === true) {
-        //alert('Usuario inicio sesión correctamente');
+        //  alert('Usuario inicio sesión correctamente');
         window.location.hash = '#/feed';
       } else {
-        //alert('Por favor revisa tu correo y verifica tu cuenta para iniciar sesión');
+        //  alert('Por favor revisa tu correo y verifica tu cuenta para iniciar sesión');
       }
       // ...
     } else {
-      //console.log('Usuario no esta registrado o no ha iniciado sesión');
+      //  console.log('Usuario no esta registrado o no ha iniciado sesión');
       // User is signed out.
     }
   });
 };
 
-//ENVIAR CORREO DE VERIFICACIÓN PARA USUARIOS CON CORREO Y CONTRASEÑA
-const verificationEmail = () => {
-  const user = firebase.auth().currentUser;
-  user.sendEmailVerification()
-    .then(() => {
-      alert('Por favor revisa tu correo y verifica tu cuenta para iniciar sesión');
-      //window.location.hash = '#/login'; // Email sent.
-    })
-    .catch((error) => {
-      // An error happened.
-    });
-};
-
-//FUNCIÓN DE INICIO DE SESIÓN PARA USUARIOS REGISTRADOS CON CORREO Y CONTRASEÑA
+//  FUNCIÓN DE INICIO DE SESIÓN PARA USUARIOS REGISTRADOS CON CORREO Y CONTRASEÑA
 export const loginUser = (email, password) => {
   firebase.auth().signInWithEmailAndPassword(email, password)
     .catch((error) => {
@@ -88,25 +94,25 @@ export const loginUser = (email, password) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       alert('Ingresa correctamente tus datos');
-      //alert('Ingresa correctamente tus datos ' + errorMessage);
-      //console.log(errorCode);
+      //  alert('Ingresa correctamente tus datos ' + errorMessage);
+      //  console.log(errorCode);
     });
 };
 
-//FUNCIÓN DE INICIO DE SESIÓN PARA USUARIOS REGISTRADOS CON GOOGLE
+//  FUNCIÓN DE INICIO DE SESIÓN PARA USUARIOS REGISTRADOS CON GOOGLE
 export const logInGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
 
   firebase.auth().signInWithPopup(provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      //const token = result.credential.accessToken;
+      //  const token = result.credential.accessToken;
       // The signed-in user info.
       const user = result.user;
       let userName = user.displayName;
       db.collection('users').doc(user.uid).get().then((doc) => {
-        if(doc.exists) {
-          //alert('Iniciaste sesión');
+        if (doc.exists) {
+          //  alert('Iniciaste sesión');
           window.location.hash = '#/feed';
         } else {
           db.collection('users').doc(user.uid).set({
@@ -114,13 +120,13 @@ export const logInGoogle = () => {
             email: user.email,
             photoURL: user.photoURL,
             userId: user.uid,
-          })
+          });
           window.location.hash = '#/feed';
         }
-      })
-      console.log(token);
-      console.log(user);
-     // ...
+      });
+      //  console.log(token);
+      //  console.log(user);
+      // ...
     })
     .catch(function (error) {
       // Handle Errors here.
@@ -134,36 +140,31 @@ export const logInGoogle = () => {
     });
 };
 
-//FUNCIÓN CERRAR SESIÓN
+//  FUNCIÓN CERRAR SESIÓN
 export const logOut = () => {
   firebase.auth().signOut()
     .then(() => {
       // Sign-out successful.
       window.location.hash = '#/home';
-      //console.log('cerrando sesión')
+      //  console.log('cerrando sesión')
     })
     .catch((error) => {
       // An error happened.
-      //console.log(error);
+      //  console.log(error);
     });
 };
 
-//CREANDO COLECCIONES Y DOCUMENTOS DE DATA
+//  CREANDO COLECCIONES Y DOCUMENTOS DE DATA
 export const addingUserData = (userName, email) => {
   const actualUser = getCurrentUser();
   db.collection('users').add({
-      name: userName,
-      email: email,
-    })
+    name: userName,
+    email: email,
+  })
     .then((docRef) => {
       console.log('Document written with ID: ', docRef.id);
     })
     .catch((error) => {
       console.error('Error adding document: ', error);
     });
-};
-
-//FUNCIÓN PARA OBETER EL USUARIO ACTUAL
-export const getCurrentUser = () => {
-  return firebase.auth().currentUser;
 };
